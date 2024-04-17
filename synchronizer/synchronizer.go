6395e24379c35bc8d1f31674ad9503c8b4a85380
Alonso Rodriguez <ARR552@users.noreply.github.com>
2024-04-17 15:22:38 +0200

    Fix/#3565 reorg (#3566)
    * fix + logs
    
    * fix loop
    
    * Revert "fix + logs"
    
    This reverts commit 39ced69339f46f145aabdf03a93279b737d02bf0.

--- synchronizer/synchronizer.go
+++ synchronizer/synchronizer.go
@@ -600,9 +600,9 @@ func (s *ClientSynchronizer) syncBlocksSequential(lastEthBlockSynced *state.Bloc
 	if lastEthBlockSynced.BlockNumber > 0 {
 		fromBlock = lastEthBlockSynced.BlockNumber
 	}
+	toBlock := fromBlock + s.cfg.SyncChunkSize
 
 	for {
-		toBlock := fromBlock + s.cfg.SyncChunkSize
 		if toBlock > lastKnownBlock.Uint64() {
 			toBlock = lastKnownBlock.Uint64()
 		}
@@ -689,7 +689,8 @@ func (s *ClientSynchronizer) syncBlocksSequential(lastEthBlockSynced *state.Bloc
 			break
 		}
 
-		fromBlock = toBlock
+		fromBlock = lastEthBlockSynced.BlockNumber
+		toBlock = toBlock + s.cfg.SyncChunkSize
 	}
 
 	return lastEthBlockSynced, nil
