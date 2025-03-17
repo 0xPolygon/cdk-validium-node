76f95e7bf87628175a48d7dfdc788b1adac02f38
Arpit Temani <temaniarpit27@gmail.com>
2025-03-17 22:04:43 +0530

    add release.yml

--- config/config.go
+++ config/config.go
@@ -96,7 +96,7 @@ type Config struct {
 	Pool pool.Config
 	// Configuration for RPC service. THis one offers a extended Ethereum JSON-RPC API interface to interact with the node
 	RPC jsonrpc.Config
-	// Configuration of service `Syncrhonizer`. For this service is also really important the value of `IsTrustedSequencer`
+	// Configuration of service `Synchronizer`. For this service is also really important the value of `IsTrustedSequencer`
 	// because depending of this values is going to ask to a trusted node for trusted transactions or not
 	Synchronizer synchronizer.Config
 	// Configuration of the sequencer service
